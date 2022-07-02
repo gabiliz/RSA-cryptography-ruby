0fa090839f6140618d0b28ed9ab328674d08100f
luizkraisch <luizkraisch22@gmail.com>
2022-07-01 22:08:55 -0300

    feat: file creation

--- decrypt.rb
+++ decrypt.rb
@@ -5,19 +5,13 @@
 require 'openssl'
 require 'base64'
 
+file_path = ARGV[0]
 private_key_file = 'private.pem';
 password = 'teste'
 
-encrypted_string = %Q{
-mN9AP/782b61geqhINqGBvrP8HuzsD9f0JoV+L+OM6laZk+4luTYFqPyZlaX
-3c/2z5mjNGwZC2XuC0RsbdMgNt/pVR3Tp3+50VlWCXK+LOTia+azFdX/pAQS
-/pplvC79snKEb22AVPq49oMxUtWR30MZMwCWFyYI2iHqNEO3do/WJyaqrqNs
-D3dtf/ZTYsWsG2Tn0jDyYe7B+gZyfAHFP7WuIG2Izk7DonwIobfhCsfnmZMJ
-QIErO9Kx7oiVChrY79e2aSwPPtmnudSubhrs7D20G4qK/B+B79CL172XWaV2
-lB78aUT7aCDMDEK5IOmXi7VbJVZBoeAsAjMicjem8w==
-}
+encrypted_string = File.read(file_path)
 
 private_key = OpenSSL::PKey::RSA.new(File.read(private_key_file),password)
 string = private_key.private_decrypt(Base64.decode64(encrypted_string))
 
-puts string, "\n"
+File.open("decrypt.txt", "w+") {|f| f.write(string) }
--- encrypt.rb
+++ encrypt.rb
@@ -5,10 +5,12 @@
 require 'openssl'
 require 'base64'
 
+puts 'Escreva algo:'
+string = gets
+
 public_key_file = 'public.pem';
-string = 'Hello World!';
 
 public_key = OpenSSL::PKey::RSA.new(File.read(public_key_file))
 encrypted_string = Base64.encode64(public_key.public_encrypt(string))
 
-puts encrypted_string
+File.open("encrypt.txt", "w+") {|f| f.write(encrypted_string) }
